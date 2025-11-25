1dc4d3fcd511afbf389020d27c4922999ea77b6c
Kandan <kandanofficial2003@gmail.com>
2025-11-25 23:57:39 +0530

    Added smooth scroll

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,18 @@ import About from "@/sections/About";
 import Contact from "@/sections/Contact";
 import Hero from "@/sections/Hero";
 import Projects from "@/sections/Projects";
+import Lenis from "lenis";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis();
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
   return (
     <main>
       <Hero />
